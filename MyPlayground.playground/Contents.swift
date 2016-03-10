00c9ce67915774938629e0e3fed2b1782cd11b5b
mikeweiwei <a5835@mac22.local>
2016-03-10 10:48:47 +0800

    maopao sort

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,28 @@
+//: Playground - noun: a place where people can play
+import Cocoa
+
+ var array = [123,234,12,346,4,75,67,234,23,1233,3,5,986,98,567,345,234,234]
+ print("排序前的值：")
+ for item in array
+ {
+     var ii = item
+     print(ii)
+     }
+
+ for var i = 0; i < array.count - 1; ++i {
+     for var j = 0; j < array.count - 1 - i; ++j{
+         if array[j] > array[j + 1] {
+             var temp = array[j + 1]
+             array[j + 1] = array[j]
+             array[j] = temp
+             }
+         }
+     }
+
+ print("排序后的值：")
+
+ for item in array
+ {
+     var ii = item
+     print(ii)
+    }
--- demo/main.swift
+++ demo/main.swift
@@ -7,7 +7,7 @@
 //
 
 import Foundation
-var a:Int,b:Int,c:Int
+/*var a:Int,b:Int,c:Int
 var charactor:Character
 a=100
 b=100
@@ -16,6 +16,36 @@ for ch in "hello".characters{
     print(ch)
 }
 print("\(c)")
-print("Hello, World!")
+print("Hello, World!")*/
+//var array = [2, 3, 4, 5]
 
+//array.sort{$0 < $1}
+//print(array)
+import Cocoa
+
+var array = [123,234,12,346,4,75,67,234,23,1233,3,5,986,98,567,345,234,234]
+print("排序前的值：")
+for item in array
+{
+    var ii = item
+    print(ii)
+}
+
+for var i = 0; i < array.count - 1; ++i {
+    for var j = 0; j < array.count - 1 - i; ++j{
+        if array[j] > array[j + 1] {
+            var temp = array[j + 1]
+            array[j + 1] = array[j]
+            array[j] = temp
+        }
+    }
+}
+
+print("排序后的值：")
+
+for item in array
+{
+    var ii = item
+    print(ii)
+}
 
